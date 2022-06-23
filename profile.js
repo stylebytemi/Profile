e779ef45ae39cb995c209ce85871924db1bd8a9e
Temiloluwa Adewale <teemaybelle@gmail.com>
2022-06-23 13:31:17 -0700

    make edit

--- profile.js
+++ profile.js
@@ -2,14 +2,17 @@
 const firstName = 'Temiloluwa'
 const lastName = 'Adewale'
 const fullName = "My name is" + " " + firstName + ' ' + lastName
-console.log(fullName);
+// console.log(fullName);
+alert(fullName);
 
 // HEIGHT
 const height = "5ft"
 const myHeight = "I am" + " " + height
-console.log(myHeight);
+// console.log(myHeight);
+alert(myHeight);
 
 // COUNTRY
 const country = "Nigeria"
 const myCountry = "I am a citizen of" + " " + country
-console.log(myCountry);
+// console.log(myCountry);
+alert(myCountry);

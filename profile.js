1da19f19af8487da5d8c67d593005cde3c2cfeb0
Temiloluwa Adewale <teemaybelle@gmail.com>
2022-06-23 02:32:21 -0700

    first commit

---
+++ profile.js
@@ -0,0 +1,15 @@
+// NAME
+const firstName = 'Temiloluwa'
+const lastName = 'Adewale'
+const fullName = "My name is" + " " + firstName + ' ' + lastName
+console.log(fullName);
+
+// HEIGHT
+const height = "5ft"
+const myHeight = "I am" + " " + height
+console.log(myHeight);
+
+// COUNTRY
+const country = "Nigeria"
+const myCountry = "I am a citizen of" + " " + country
+console.log(myCountry);
